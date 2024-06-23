133a181fd7a25e2467c9b0aab5ec00c366091251
53rNa <53rNa76@gmail.com>
2024-06-23 10:29:42 +0300

    Add files via upload
    Решение к практическому заданию по уроку "Строки и индексация строк"

---
+++ homework1.py
@@ -0,0 +1,8 @@
+#Практическое задание по уроку "Строки и индексация строк"
+
+example = 'Python'
+print(example[0])
+print(example[-1])
+print(example[3:6])
+print (example[::-1])
+print (example[1::2])
